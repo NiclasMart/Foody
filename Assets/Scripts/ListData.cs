6cd1a08471d4211c425c86cf94632f0e677c4e6c
Niclas Mart <salcintram07@web.de>
2025-01-26 21:17:09 +0100

    shopping list items can now be added to different groups

--- Assets/Scripts/ListData.cs
+++ Assets/Scripts/ListData.cs
@@ -5,89 +5,94 @@ using UnityEngine;
 
 public class ListData : MonoBehaviour
 {
-  public List<Recipe> recipes = new List<Recipe>();
-  public List<string> foods = new List<string>();
-  public Dictionary<string, int> purchases = new Dictionary<string, int>();
-  public static ListData instance = null;
+    public List<Recipe> recipes = new List<Recipe>();
+    public List<string> foods = new List<string>();
+    public Dictionary<string, int[]> purchases = new Dictionary<string, int[]>(); //first integer is amount, second is group
+    public int activePurchaseGroup = -1; // saves the current sorting group in th purchase list until the app is closed
+    public static ListData instance = null;
 
-  private void Awake()
-  {
-    if (instance != null)
+    private void Awake()
     {
-      Destroy(this);
-      return;
-    }
-    instance = this;
-    DontDestroyOnLoad(this);
-  }
-
-  public void AddRecipe(Recipe newRecipe)
-  {
-    recipes.Add(newRecipe);
-  }
-
-  public void DeleteRecipe(Recipe recipe)
-  {
-    if (recipe == null) return;
-    recipes.Remove(recipe);
-  }
-
-  public void SetRecipes(List<Recipe> list)
-  {
-    recipes = list;
-  }
-
-  public void SaveRecipeList()
-  {
-    SavingSystem.Save(recipes, "RecipeList");
-  }
-
-  public void LoadRecipeList()
-  {
-    recipes = (List<Recipe>)SavingSystem.Load("RecipeList");
-
-    //Todo: can be deleted after all recipes have an ID
-    foreach(var recipe in recipes)
-    {
-        if (recipe.ID == null)
+        if (instance != null)
         {
-            recipe.ID = Guid.NewGuid().ToString();
-            Debug.Log("Generated new gui");
+            Destroy(this);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this);
+    }
+
+    public void AddRecipe(Recipe newRecipe)
+    {
+        recipes.Add(newRecipe);
+    }
+
+    public void DeleteRecipe(Recipe recipe)
+    {
+        if (recipe == null) return;
+        recipes.Remove(recipe);
+    }
+
+    public void SetRecipes(List<Recipe> list)
+    {
+        recipes = list;
+    }
+
+    public void SaveRecipeList()
+    {
+        SavingSystem.Save(recipes, "RecipeList");
+    }
+
+    public void LoadRecipeList()
+    {
+        recipes = (List<Recipe>)SavingSystem.Load("RecipeList");
+
+        //Todo: can be deleted after all recipes have an ID
+        foreach (var recipe in recipes)
+        {
+            if (recipe.ID == null)
+            {
+                recipe.ID = Guid.NewGuid().ToString();
+                Debug.Log("Generated new gui");
+            }
+        }
+
+        if (recipes == null) recipes = new List<Recipe>();
+    }
+
+    public void SaveFoodList()
+    {
+        SavingSystem.Save(foods, "FoodList");
+    }
+
+    public void LoadFoodList()
+    {
+        foods = (List<string>)SavingSystem.Load("FoodList");
+        if (foods == null) foods = new List<string>();
+    }
+
+    public void SaveShoppingList()
+    {
+        SavingSystem.Save(purchases, "ShoppingList");
+    }
+
+    public void LoadShoppingList()
+    {
+        purchases = (Dictionary<string, int[]>)SavingSystem.Load("ShoppingList");
+        if (purchases == null) purchases = new Dictionary<string, int[]>();
+    }
+
+    public void AddNewItemToShoppingCard(string name, int amount, int group)
+    {
+        if (purchases.ContainsKey(name))
+        {
+            instance.purchases[name][0] += amount;
+        }
+        else
+        {
+            purchases.Add(name, new int[] { amount, group });
         }
     }
-
-    if (recipes == null) recipes = new List<Recipe>();
-  }
-
-  public void SaveFoodList()
-  {
-    SavingSystem.Save(foods, "FoodList");
-  }
-
-  public void LoadFoodList()
-  {
-    foods = (List<string>)SavingSystem.Load("FoodList");
-    if (foods == null) foods = new List<string>();
-  }
-
-  public void SaveShoppingList()
-  {
-    SavingSystem.Save(purchases, "ShoppingList");
-  }
-
-  public void LoadShoppingList()
-  {
-    purchases = (Dictionary<string, int>)SavingSystem.Load("ShoppingList");
-    if (purchases == null) purchases = new Dictionary<string, int>();
-  }
-
-  public void AddNewItemToShoppingCard(string name, int amount)
-  {
-    if (purchases.ContainsKey(name))
-      ListData.instance.purchases[name] += amount;
-    else
-      purchases.Add(name, amount);
-  }
 
 
 }
--- Assets/Scripts/ShoppingListHandler.cs
+++ Assets/Scripts/ShoppingListHandler.cs
@@ -3,55 +3,84 @@ using System.Collections.Generic;
 using TMPro;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShoppingListHandler : ListHandler
 {
-  [SerializeField] ShoppingSlot shoppingSlot;
-  [SerializeField] InputFunctionality purchaseIn;
+    public static readonly Color[] groupColor = new Color[] { new Color(1f, 1f, 1f), new Color(0.88f, 0.73f, 0.33f), new Color(0.88f, 0.33f, 0.41f), new Color(0.33f, 0.63f, 0.88f) };
+    [SerializeField] ShoppingSlot shoppingSlot;
+    [SerializeField] InputFunctionality purchaseIn;
+    [SerializeField] Transform displayGroupSwitchIcon;
 
-  private void Awake()
-  {
-    purchaseIn.GetComponent<TMP_InputField>().onSubmit.AddListener(ShowOnSubmit);
-  }
-
-  public override void ShowCompleteList()
-  {
-    ShowDictionary(ListData.instance.purchases);
-  }
-
-  public void ShowDictionary(Dictionary<string, int> list)
-  {
-    base.DeleteList();
-
-    foreach (KeyValuePair<string, int> item in list.OrderBy(key => key.Key))
+    private void Awake()
     {
-      ShoppingSlot slot = Instantiate(shoppingSlot, listTransform);
-      slot.Initialize(item);
+        purchaseIn.GetComponent<TMP_InputField>().onSubmit.AddListener(ShowOnSubmit);
     }
-  }
 
-  public void ShowOnSubmit(string msg)
-  {
-    StartCoroutine(UpdateList());
-  }
+    public override void ShowCompleteList()
+    {
+        int currentDisplayGroup = ListData.instance.activePurchaseGroup;
+        if (currentDisplayGroup == -1) ShowDictionary(ListData.instance.purchases);
+        else
+        {
+            var purchaseDisplayList = ListData.instance.purchases.Where(x =>
+                x.Value[1] == currentDisplayGroup || currentDisplayGroup == -1)
+                .ToDictionary(x => x.Key, x => x.Value);
+            ShowDictionary(purchaseDisplayList);
+        }
+        SetDisplayGroupSwitchIconColor();
+    }
 
-  public void DeleteItemFromList(string name)
-  {
-    ListData.instance.purchases.Remove(name);
-    ListData.instance.SaveShoppingList();
-    ShowCompleteList();
-  }
+    public void ShowDictionary(Dictionary<string, int[]> list)
+    {
+        base.DeleteList();
 
-  public void DeleteCompleteList()
-  {
-    ListData.instance.purchases = new Dictionary<string, int>();
-    ListData.instance.SaveShoppingList();
-    ShowCompleteList();
-  }
+        foreach (var item in list.OrderBy(key => key.Key))
+        {
+            ShoppingSlot slot = Instantiate(shoppingSlot, listTransform);
+            slot.Initialize(item);
+        }
+    }
 
-  IEnumerator UpdateList()
-  {
-    yield return new WaitForEndOfFrame();
-    ShowCompleteList();
-  }
+    public void ShowOnSubmit(string msg)
+    {
+        StartCoroutine(UpdateList());
+    }
+
+    public void DeleteItemFromList(string name)
+    {
+        ListData.instance.purchases.Remove(name);
+        ListData.instance.SaveShoppingList();
+        ShowCompleteList();
+    }
+
+    public void DeleteCompleteList()
+    {
+        ListData.instance.purchases = new Dictionary<string, int[]>();
+        ListData.instance.SaveShoppingList();
+        ShowCompleteList();
+    }
+
+    public void SwitchDisplayGroup()
+    {
+        ListData.instance.activePurchaseGroup++;
+        if (ListData.instance.activePurchaseGroup > 3) ListData.instance.activePurchaseGroup = -1;
+        ShowCompleteList();
+    }
+
+    private void SetDisplayGroupSwitchIconColor()
+    {
+        Color displayColor = ListData.instance.activePurchaseGroup == -1 ? Color.black : groupColor[ListData.instance.activePurchaseGroup];
+        foreach (var image in displayGroupSwitchIcon.GetComponentsInChildren<Image>())
+        {
+            Debug.Log(displayColor);
+            image.color = displayColor;
+        }
+    }
+
+    IEnumerator UpdateList()
+    {
+        yield return new WaitForEndOfFrame();
+        ShowCompleteList();
+    }
 }
